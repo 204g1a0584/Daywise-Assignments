148c2d555623c35c8e99b1ea948f8c8439203abd
SAI CHARAN REDDY <79386814+204g1a0584@users.noreply.github.com>
2023-10-30 11:29:46 +0530

    Completed,delete features added

--- ToDo List/index.js
+++ ToDo List/index.js
@@ -14,20 +14,35 @@ function addtask() {
     var button2 = document.createElement("button");
     button1.className = "task-done";
     button2.className = 'delete';
-    button1.innerHTML = " ✅ ";
+    button1.innerHTML = "✓";
     button2.innerHTML = " ✖ ";
-
-    var taskText = document.createTextNode(text);
+    var span= document.createElement('span')
+    span.textContent=text
+ //   var taskText = document.createTextNode(text);
 
     x.appendChild(button1);
-    x.appendChild(taskText);
+    x.appendChild(span)
+   // x.appendChild(taskText);
     x.appendChild(button2);
 
     // Add a click event listener to the delete button
     button1.addEventListener("click", function() {
         var listItem = document.getElementById(x.id);
-        listItem.remove();
+        var button1 = listItem.querySelector(".task-done"); // Get the "✅" button
+        var text = listItem.querySelector('span'); // Get the text
+    
+        if (text.style.textDecoration === "line-through") {
+            text.style.textDecoration = "none"; // Remove the strikethrough
+            button1.innerHTML = "✓ "; // Change the button symbol back
+        } else {
+            text.style.textDecoration = "line-through"; // Apply strikethrough style to the text
+            button1.innerHTML = " ✅ "; // Change the button symbol to a checkmark
+        }
     });
+    
+    
+    
+    
 
 
     button2.addEventListener("click", function() {
